2ee2711d96645f2cc889a56688804a931a432806
indgit <pankaj.azm2@gmail.com>
2023-08-30 16:56:30 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -71,12 +71,6 @@ export default function Home() {
             >
               Hello 👋
             </h1>
-            <h1
-              ref={textTwo}
-              className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5"
-            >
-              {data.headerTaglineTwo}
-            </h1>
                 <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5">
                 Welcome in Regolith Classes
                 </h1>
